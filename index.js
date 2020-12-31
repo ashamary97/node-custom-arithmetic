50e67317900b77591c13e92fa8abbc7be4074a20
ashamary97 <thomasmaryasha1997@gmail.com>
2021-01-01 01:35:49 +0530

    custom module

---
+++ index.js
@@ -0,0 +1,11 @@
+const addition=require("./arithmetic")
+var x=20
+var y=10
+var c=addition.addNum(x,y)
+console.log(c)
+var c=addition.subNum(x,y)
+console.log(c)
+var c=addition.mulNum(x,y)
+console.log(c)
+var c=addition.divNum(x,y)
+console.log(c)
